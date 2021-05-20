63935eadaa52d751d264ea0285c5babcfaf360cf
Ben Gao <baiyungao@gmail.com>
2021-05-19 21:47:51 -0400

    update

--- src/app/services/session.service.ts
+++ src/app/services/session.service.ts
@@ -1,147 +1,247 @@
 import { NumberSymbol } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Plugins, Capacitor,GeolocationPosition} from '@capacitor/core';
+import { Plugins, Capacitor, GeolocationPosition } from '@capacitor/core';
 import { AccessService } from './access.service';
 
 const { Geolocation } = Plugins;
 
 export interface Drive {
-  id:string;
+  id?: string;
   email: string;
   start: Date;
   end: Date;
-  driving:{
+  drive: {
     daytime: number;  //driving time in second during daytime;
     nighttime: number; //driving time in second during nighttime;
-    distance: Number; //driving distance in meters;
+    distance: number; //driving distance in meters caculated through speed;
+    distanceB: number; //drive distance caculated through location;
   }
   idle: number;  //idling time during the drive;
-  locations:GeolocationPosition[];
+  locations: Location[];
 }
 
+export interface Location {
+  timestamp: number;
+  /**
+   * The GPS coordinates along with the accuracy of the data
+   */
+  /**
+   * Latitude in decimal degrees
+   */
+  latitude: number;
+  /**
+   * longitude in decimal degrees
+   */
+  longitude: number;
+  /**
+   * Accuracy level of the latitude and longitude coordinates in meters
+   */
+  accuracy: number;
+  /**
+   * Accuracy level of the altitude coordinate in meters (if available)
+   */
+  altitudeAccuracy?: number;
+  /**
+   * The altitude the user is at (if available)
+   */
+  altitude?: number;
+  /**
+   * The speed the user is traveling (if available)
+   */
+  speed?: number;
+  /**
+   * The heading the user is facing (if available)
+   */
+  heading?: number;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class SessionService {
-   
-  public locations:GeolocationPosition[]=[];
+
+  public locations: Location[] = [];
   lastLocation: GeolocationPosition;
-  currentDrive:Drive;
-  ideling: number;
-   
-  constructor(public access:AccessService) { }
+  currentDrive: Drive = null;
+  ideling: number = 0;
+  paused: boolean;
+
+  constructor(private http: HttpClient, public access: AccessService) { }
 
 
 
-    //start a user drive session
-    public async startDrive()
-    {
-        this.currentDrive = this.getDrive();
-        this.currentDrive.email = this.access.currentUser.email;
-        this.locations=[];
-        let id=this.access.currentUser.email + new Date().getMilliseconds();
-        this.currentDrive.start= new Date();
-        this.currentDrive.locations = this.locations;
+  //start a user drive session
+  public async startDrive() {
+    console.log("start drive");
+    this.currentDrive = this.getDrive();
+    this.currentDrive.email = this.access.currentUser.email;
+    this.locations = [];
+    let id = this.access.currentUser.email + new Date().getMilliseconds();
+    this.currentDrive.start = new Date();
+    this.currentDrive.locations = this.locations;
 
-        //start log;
+    //start log;
+    this.start();
+
+  }
+
+  /**
+   * if the new location is different from the last location, 
+   * adding driving time; otherwise adding to idle time
+   * @param location 
+   * 
+   * The geolocation data are the data per sceond;
+   */
+  public async trace(location: GeolocationPosition) {
+    this.locations.unshift(this.getLocation(location));
+    if (this.lastLocation != null) {
+      //location changed;
+      if ((this.lastLocation.coords.latitude != location.coords.latitude)
+        || (this.lastLocation.coords.longitude != location.coords.longitude)) {
+
+        this.ideling = 0;
+        //add distance caculation here;
+        this.currentDrive.drive.distance += this.caculateDistance(
+          location.coords.latitude, location.coords.longitude, this.lastLocation.coords.latitude,
+          this.lastLocation.coords.longitude);
+
+      }
+      else {
+        this.ideling++;
+        console.log("idling:" + this.ideling);
+      }
 
     }
 
-    /**
-     * if the new location is different from the last location, 
-     * adding driving time; otherwise adding to idle time
-     * @param location 
-     * 
-     * The geolocation data are the data per sceond;
-     */
-    public async trace(location:GeolocationPosition){
-      this.locations.unshift(location);
-        if (this.lastLocation !=null) {
-          if (!((this.lastLocation.coords.latitude == location.coords.latitude) 
-               && (this.lastLocation.coords.longitude == location.coords.longitude))) {
-                 
-                this.ideling = 0;
-                 //add distance caculation here;
-
-                 }
-                 else {
-                   this.ideling++;
-                   console.log("idling:" + this.ideling);
-                 }
-
-        }
-
-        //if the viechel is not moving for 5 seconds, start to count on idle time;
-        if (this.ideling > 5) {
-          this.currentDrive.idle ++;
-        }
-
-        //apply logic to determine daytime or night time
-        this.currentDrive.driving.daytime ++;
-        console.log("moving:" + this.currentDrive.driving.daytime);
-                 
-
-       //update the lastLocation
-       this.lastLocation = location;
+    //if the viechel is not moving for 5 seconds, start to count on idle time;
+    if (this.ideling > 5) {
+      this.currentDrive.idle++;
     }
 
-    //Proto type method
-    getDrive():Drive{
-      let drive: Drive = {id: 'default', 
-                 email: '',
-                 start: null, end: null, 
-                 driving: {daytime: 0, nighttime:0, distance: 0},
-                 idle: 0,
-                 locations: []
-                };
+    //apply logic to determine daytime or night time
+    this.currentDrive.drive.daytime++;
+    this.currentDrive.drive.distanceB += location.coords.speed;
+    console.log("moving:" + this.currentDrive.drive.daytime);
 
-      return drive;          
+
+    //update the lastLocation
+    this.lastLocation = location;
+  }
+
+  /**
+   * Save the current drive to the cloud;
+   * https://twheeldb.azurewebsites.net/api/adddrive
+   * 
+   * body //drive
+   */
+  save() {
+    const saveUrl = "https://twheeldb.azurewebsites.net/api/adddrive"
+    const body = this.currentDrive;
+    this.currentDrive.end = new Date();
+    console.log(JSON.stringify(body));
+    console.log(JSON.stringify(this.lastLocation));
+    return this.http.post(saveUrl, body,);
+  }
+
+  reset() {
+    this.currentDrive = null;
+    this.paused = false;
+  }
+
+
+
+
+  //Proto type method
+  getDrive(): Drive {
+    let drive: Drive = {
+      email: '',
+      start: null, end: null,
+      drive: { daytime: 0, nighttime: 0, distance: 0, distanceB: 0 },
+      idle: 0,
+      locations: []
+    };
+
+    return drive;
+  }
+
+
+
+  getLocation(loc: GeolocationPosition):Location{
+
+    let location: Location;
+    location = {
+      timestamp: loc.timestamp,
+      latitude: loc.coords.latitude,
+      longitude: loc.coords.longitude,
+      altitude:loc.coords.altitude,
+      accuracy: loc.coords.accuracy,
+      altitudeAccuracy: loc.coords.altitudeAccuracy,
+      speed: loc.coords.speed,
+      heading: loc.coords.heading
     }
-    
-    // Timer logic
-    //-------------------------------------
 
-    interval;
+
+    return location;
+  }
+
+  // Timer logic
+  //-------------------------------------
+
+  interval;
 
 
   async getCurrentPosition() {
     const coordinates = await Geolocation.getCurrentPosition();
     this.trace(coordinates);
-    }
-  
+  }
+
   //start tracing the movement or resume the tracing
-  public async start(){
+  public async start() {
     this.interval = setInterval(() => {
-      this.getCurrentPosition();      
-    },1000);
-    console.log("started:" + this.interval);
+      this.getCurrentPosition();
+    }, 1000);
+    this.paused = false;
   }
 
   //Paulse the tracing of movement
-  public async paulse(){
+  public async pause() {
     clearInterval(this.interval);
-    console.log("pausled:" + this.interval);
-  } 
-
-  /**
-   *  The session has multipe status
-   * 
-   *  1. not active drive;
-   *  2. active drive in tracing;
-   *  3. active drive pausled 
-   */
-
-  hasActiveDrive():boolean{
-    return this.currentDrive != null;
-  }
-  
-  isPulsed(){
-    this.interval
+    this.paused = true;
   }
 
 
- }
+
+  private caculateDistance
+    (lat1: number, lon1: number,
+      lat2: number, lon2: number) {
+
+    // The math module contains a function
+    // named toRadians which converts from
+    // degrees to radians.
+    lon1 = lon1 * Math.PI / 180;
+    lon2 = lon2 * Math.PI / 180;
+    lat1 = lat1 * Math.PI / 180;
+    lat2 = lat2 * Math.PI / 180;
+
+    // Haversine formula
+    let dlon = lon2 - lon1;
+    let dlat = lat2 - lat1;
+    let a = Math.pow(Math.sin(dlat / 2), 2)
+      + Math.cos(lat1) * Math.cos(lat2)
+      * Math.pow(Math.sin(dlon / 2), 2);
+
+    let c = 2 * Math.asin(Math.sqrt(a));
+
+    // Radius of earth in kilometers. Use 3956
+    // for miles
+    let r = 6371;
+
+    // calculate the result return in meters
+    return (c * r) * 1000;
+  }
+
+}
 
 
 
--- src/app/tab-home/tab-home.page.ts
+++ src/app/tab-home/tab-home.page.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { TracingService } from '../services/tracing.service';
 import { SessionService } from '../services/session.service';
 import { AccessService } from '../services/access.service';
 
@@ -10,26 +9,52 @@ import { AccessService } from '../services/access.service';
 })
 export class TabHomePage implements OnInit {
 
-  constructor(public tracingService: TracingService, 
-     public sessionService: SessionService,
-     public access: AccessService) {}
+  constructor(public sessionService: SessionService,
+     public access: AccessService) {
+       console.log('login user:' + JSON.stringify(this.access.currentUser));
+     }
 
     start(){
     this.sessionService.startDrive();
     }
 
     resume(){
-    this.tracingService.stop();
+    this.sessionService.start();
     }
 
-    paulse(){
+    pause(){
+    this.sessionService.pause();
+    }
+    cancel(){
 
     }
 
     save(){
+      this.pause();
+      this.sessionService.save().subscribe((data)=>{
+        console.log("save the drive success");
+        this.sessionService.reset();
+      })
 
     }
 
+
+    /**
+   *  The session has multipe status
+   * 
+   *  1. not active drive;
+   *  2. active drive in tracing;
+   *  3. active drive pausled 
+   */
+
+  started():boolean{
+    //console.log(" can start:" + this.sessionService.currentDrive)
+    return this.sessionService.currentDrive != null;
+  }
+  
+  isPaused(){
+    return this.sessionService.paused;
+  }
 
   ngOnInit() {
   }
